82e01b0e25ddcd98e777fbdf3cda97523802992f
Sean <seanhuang1228@gmail.com>
2024-03-28 21:14:39 +0800

    mod-nvim: lazy load codeium

--- nvim/.config/nvim/lua/custom/plugins/codeium.lua
+++ nvim/.config/nvim/lua/custom/plugins/codeium.lua
@@ -4,6 +4,8 @@ return {
     'nvim-lua/plenary.nvim',
     'hrsh7th/nvim-cmp',
   },
+  -- run :Lazy load codeium.nvim to load
+  lazy = true,
   config = function()
     require('codeium').setup {}
   end,
