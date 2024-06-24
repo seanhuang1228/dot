41aa9e3bb9424e1ddc81dd20f5d68dde092122c1
Sean <seanhuang1228@gmail.com>
2024-06-25 07:29:35 +0800

    modify: shortcut fraction type of vimtex

--- nvim/.config/nvim/lua/custom/plugins/vimtex.lua
+++ nvim/.config/nvim/lua/custom/plugins/vimtex.lua
@@ -1,7 +1,21 @@
 return {
   'lervag/vimtex',
   init = function()
+    vim.g.vimtex_toggle_fractions = {
+      INLINE = 'dfrac',
+      frac = 'INLINE',
+      dfrac = 'INLINE',
+    }
     -- vim.g.vimtex_compiler_method =
     -- Use init for configuration, don't use the more common "config".
+  end,
+  config = function()
+    vim.fn['vimtex#imaps#add_map'] {
+      lhs = 'b',
+      rhs = 'vimtex#imaps#style_math("textbf")',
+      expr = 1,
+      leader = '#',
+      wrapper = 'vimtex#imaps#wrap_math',
+    }
   end,
 }
