074df9ea4782a330c692f6faf0a2da3414261198
seanhuang1228 <seanhuang1228@gmail.com>
2026-07-27 17:45:49 +0800

    feat(nvim): add python formatters and fix format on save

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -526,15 +526,13 @@ require('lazy').setup({
         format_on_save = {
           timeout_ms = 500,
           lsp_fallback = true,
-          filter = function()
-            return '$EXTENSION' == '.py'
-          end,
         },
         formatters_by_ft = {
           lua = { 'stylua' },
           -- Conform can also run multiple formatters sequentially
           cpp = { 'uncrustify' },
           rust = { 'rustfmt' },
+          python = { 'ruff_format', 'isort' },
           --
           -- You can use a sub-list to tell conform to run *until* a formatter
           -- is found.
