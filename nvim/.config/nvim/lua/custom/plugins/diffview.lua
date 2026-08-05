b4522c14b28e63f433309a93591ec243f34ca0af
Sean <seanhuang1228@gmail.com>
2026-08-05 18:01:30 +0800

    clean up

--- nvim/.config/nvim/lua/custom/plugins/diffview.lua
+++
@@ -1,8 +0,0 @@
-return {
-  'sindrets/diffview.nvim',
-  config = function()
-    local diffview = require 'diffview'
-
-    vim.keymap.set('n', '<leader>gd', diffview.open, { desc = '[G]earch [D]iff' })
-  end,
-}
